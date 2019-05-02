0178b82c5cd30822dc93b8f54a7897be2ebad10d
Anibal Santiago <anibsanmor@gmail.com>
2019-05-02 12:41:47 -0500

    add flayway

---
+++ src/main/resources/db/migration/V1__Create_Table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE customer (
+	id VARCHAR(255) PRIMARY KEY ,
+	age integer,
+	name VARCHAR(255)
+);
---
+++ src/main/resources/db/migration/V2__Insert_row.sql
@@ -0,0 +1,3 @@
+insert into customer VALUES(uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),29,'Anibal Santiago');
+insert into customer VALUES(uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),30,'Luis Manuel ');
+insert into customer VALUES(uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),21,'Nelly Santiago');
